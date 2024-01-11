c6cbecdce236307c29e4b7789601b345a969da8f
Carlos Arturo Moreno Tabares <carlox216@gmail.com>
2024-01-11 10:48:11 -0500

    docs: add docstring on FilterLogLevelSep object

--- project_example/project_example/logger/filter_log_level_sep.py
+++ project_example/project_example/logger/filter_log_level_sep.py
@@ -4,13 +4,35 @@ from typing import Optional
 
 
 class FilterLogLevelSep(logging.Filter):
+    """
+    Limit log output to log levels located at
+    :attr:`FilterLogLevelSep._filter_levels`
+
+    :param _filter_levels: levels name in full caps that are allowed to emit\
+    log
+    :type _filter_levels: list[str], list of levels in str uppercase
+    """
+
     _filter_levels: list[str]
+    """
+    levels name in full caps that are allowed to emit log
+    """
 
     def __init__(
         self,
         name: str = "",
         filter_levels: Optional[list[str]] = None,
     ):
+        """
+        Initialize filter Log level separator
+
+        :param name: name of filter, required on related object\
+        :class:`logging.Filter`, defaults to ""
+        :type name: str, optional
+        :param filter_levels: levels name in full caps that are allowed to\
+        emit log, defaults to empty list through value None
+        :type filter_levels: Optional[list[str]], optional
+        """
         super(FilterLogLevelSep, self).__init__(name=name)
         self._filter_levels = filter_levels or list()
 
