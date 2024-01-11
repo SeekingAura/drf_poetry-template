b5cdfc4fa76104ac7883aee1d7251fc6416e348e
Carlos Arturo Moreno Tabares <carlox216@gmail.com>
2024-01-11 10:14:19 -0500

    type: fill typing on FilterLogLevelSep object

--- project_example/project_example/logger/filter_log_level_sep.py
+++ project_example/project_example/logger/filter_log_level_sep.py
@@ -1,14 +1,20 @@
 import logging
 import logging.config
+from typing import Optional
 
 
 class FilterLogLevelSep(logging.Filter):
-    def __init__(self, filter_levels=None):
-        super(FilterLogLevelSep, self).__init__()
-        self._filter_levels = filter_levels
+    _filter_levels: list[str]
 
-    #
-    def filter(self, record):
+    def __init__(
+        self,
+        name: str = "",
+        filter_levels: Optional[list[str]] = None,
+    ):
+        super(FilterLogLevelSep, self).__init__(name=name)
+        self._filter_levels = filter_levels or list()
+
+    def filter(self, record: logging.LogRecord) -> bool:
         if record.levelname in self._filter_levels:
             return True
         return False
